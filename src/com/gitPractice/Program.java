b3f3d21db4b9ad2ce6741b958b48b047662bc599
J1ggy2B <watsongraeme418@gmail.com>
2025-01-06 11:56:58 +0000

    initial commit of Program.java

---
+++ src/com/gitPractice/Program.java
@@ -0,0 +1,17 @@
+
+
+package com.gitPractice;
+
+
+public class Program {  
+
+
+	public static void main(String[] args) {              
+		
+			System.out.println("Hello World"); 
+		     
+	}
+}
+
+
+
